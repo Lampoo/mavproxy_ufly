a25a7c973fc30008230226e4f9bbb91583f9cba6
Yonghua <yonghua.zheng@goertek.com>
2023-04-19 22:03:51 +0800

    Refine the implementation
    Making the GSTPipeline a subclass of Thread and implement
    the run method for status maintenance.

--- app/rtmp.py
+++ app/rtmp.py
@@ -1,4 +1,5 @@
 import sys, os, time, signal, multiprocessing
+from threading import Thread
 from optparse import OptionParser
 
 import gi
@@ -6,18 +7,11 @@ gi.require_version("Gst", '1.0')
 from gi.repository import Gst, GLib
 Gst.init(None)
 
-class GSTPipeline(object):
+class GSTPipeline(Thread):
     def __init__(self):
+        super(GSTPipeline, self).__init__()
         self.pipeline = None
-        self.running = False
-        self.error_handler = None
-        self.eos_handler = None
-
-    def set_error_handler(self, fn):
-        self.error_handler = fn
-
-    def set_eos_handler(self, fn):
-        self.eos_handler = fn
+        self.time_to_quit = False
 
     def source(self):
         return self.pipeline.get_by_name('source')
@@ -29,63 +23,20 @@ class GSTPipeline(object):
         return self.pipeline.get_by_name('sink')
 
     def set_location(self, uri):
-        self.rtmpsink().set_property('location', uri)
+        self.sink().set_property('location', uri)
 
-    def start(self):
-        if not self.running:
-            self.running = True
-            bus = self.pipeline.get_bus()
-            bus.enable_sync_message_emission()
-            bus.connect("sync-message", self.on_bus_message, self)
+    def run(self):
+        delay = 0
+        while not self.time_to_quit:
+            if delay > 0:
+                time.sleep(delay)
             self.pipeline.set_state(Gst.State.PLAYING)
-
-    def stop(self):
-        if self.running:
-            bus = self.pipeline.get_bus()
-            bus.disable_sync_message_emission()
+            delay = self.process_message()
             self.pipeline.set_state(Gst.State.NULL)
-            self.running = False
 
-    def on_bus_message(self, bus, message, userdata):
-        if message.type == Gst.MessageType.ERROR:
-            err, debug = message.parse_error()
-            print("Error received from element %s: %s" % (message.src.get_name(), err))
-            print("Debug information %s" % debug)
-            self.handle_error()
-        elif message.type == Gst.MessageType.EOS:
-            print("End-of-stream")
-            self.handle_eos()
-        elif message.type == Gst.MessageType.STATE_CHANGED:
-            if isinstance(message.src, Gst.Pipeline):
-                old_state, new_state, pending_state = message.parse_state_changed()
-                print("Pipeline state changed from %s to %s." % (old_state.value_nick, new_state.value_nick))
-        #else:
-        #    print("Unexpected message received.")
-
-        return True
-
-    def handle_error(self):
-        r = False
-        if self.error_handler is not None:
-            r = self.error_handler()
-        if not r:
-            self.stop()
-
-    def handle_eos(self):
-        r = False
-        if self.eos_handler is not None:
-            r = self.eos_handler()
-        if not r:
-            self.stop()
-
-    def main_loop(self, ev_exit):
+    def process_message(self, timeout_ns = Gst.CLOCK_TIME_NONE):
         bus = self.pipeline.get_bus()
-        timeout_ns = 10000000
-
-        # start pipeline
-        self.pipeline.set_state(Gst.State.PLAYING)
-
-        while not ev_exit.is_set():
+        while True:
             message = bus.timed_pop_filtered(timeout_ns, Gst.MessageType.ANY)
             if message:
                 if message.type == Gst.MessageType.ERROR:
@@ -93,23 +44,25 @@ class GSTPipeline(object):
                     print("Error received from element %s: %s" % (
                         message.src.get_name(), err))
                     print("Debugging information: %s" % debug)
-                    break
+                    return 1
+                elif message.type == Gst.MessageType.EOS:
+                    print("End-Of-Stream reached.")
+                    return 1
                 elif message.type == Gst.MessageType.WARNING:
                     warning, debug = message.parse_warning()
                     print(warning, debug)
-                elif message.type == Gst.MessageType.EOS:
-                    print("End-Of-Stream reached.")
-                    break
                 elif message.type == Gst.MessageType.STATE_CHANGED:
                     if isinstance(message.src, Gst.Pipeline):
                         old_state, new_state, pending_state = message.parse_state_changed()
                         print("Pipeline state changed from %s to %s." %
-                              (old_state.value_nick, new_state.value_nick))
-                else:
-                    print("Unexpected message received.")
+                            (old_state.value_nick, new_state.value_nick))
+            else:
+                # Timeout
+                return 0
 
-        # Stop pipline
-        self.pipeline.set_state(Gst.State.NULL)
+    def stop(self):
+        self.time_to_quit = True
+        self.pipeline.send_event(Gst.Event.new_eos())
 
     @staticmethod
     def from_uri(uri):
@@ -128,10 +81,12 @@ class VideotestPipeline(GSTPipeline):
         super(VideotestPipeline, self).__init__()
         if uri.startswith('videotestsrc://'):
             pattern = uri[15:]
+        else:
+            pattern = ''
         if pattern is None or pattern == '':
             pattern = 'smpte'
         self.pipeline = Gst.parse_launch(
-            'videotestsrc pattern={} name=source ! x264enc tune=zerolatency rc-lookahead=5 ! flvmux streamable=true ! rtmpsink name=sink'.format(pattern)
+            'videotestsrc pattern={} name=source ! tee name=t ! x264enc tune=zerolatency rc-lookahead=5 ! flvmux streamable=true ! rtmpsink sync=false live=1 name=sink'.format(pattern)
         )
 
 
@@ -139,29 +94,8 @@ class RTSPPipeline(GSTPipeline):
     def __init__(self, uri):
         super(RTSPPipeline, self).__init__()
         self.pipeline = Gst.parse_launch(
-            'rtspsrc name=source location={} latency=0 udp-reconnect=1 timeout=5000000 ! h264parse ! rtph264depay ! flvmux streamable=true ! rtmpsink sync=false name=sink'.format(uri)
+            'rtspsrc name=source location={} latency=0 udp-reconnect=1 timeout=5000000 ! rtph264depay ! flvmux streamable=true ! rtmpsink sync=false live=1 name=sink'.format(uri)
         )
-
-
-class RtmpHandler(object):
-    def __init__(self, uri):
-        self.pipeline = GSTPipeline.from_uri(uri)
-
-    def start(self, uri):
-        self.pipeline.stop()
-
-        sink = self.pipeline.sink()
-        sink.set_property('location', uri)
-
-        self.pipeline.start()
-
-    def stop(self):
-        self.pipeline.stop()
-
-    def update(self):
-        if self.pipeline.running:
-            # check pipeline status
-            pass
 
 
 class Application(object):
@@ -177,13 +111,15 @@ class Application(object):
         signal.signal(signal.SIGINT, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-        rtmpHandler = RtmpHandler(opts.input)
-        rtmpHandler.start(opts.stream_url)
+        pipeline = GSTPipeline.from_uri(opts.input)
+        pipeline.set_location(opts.stream_url)
+        pipeline.start()
 
         while not self.ev_exit.is_set():
             time.sleep(1)
 
-        rtmpHandler.stop()
+        pipeline.stop()
+        pipeline.join()
 
     def exit_gracefully(self, signum, frame):
         print("exit_gracefully\n")
